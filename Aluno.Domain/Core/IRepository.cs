43b3eebb0dd46432919b02292c1394f3b8b7d492
Eduardo Aguiar <eduardo.aguiar@k2partnering.com>
2020-10-28 17:55:22 -0300

    Criando Core

---
+++ Aluno.Domain/Core/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Aluno.Domain.Core
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : BaseEntity
+    {
+        void Adicionar(TEntity obj);
+        TEntity ObterPorId(Guid id);
+        IEnumerable<TEntity> ObterTodos();
+        void Atualizar(TEntity obj);
+        void Remover(Guid id);
+        IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicate);
+        int Commit();
+    }
+}
